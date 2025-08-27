59e11743f8977d6380a58f188d6e48d5add20a96
Mariana Santana <marimoon1996@outlook.com>
2025-08-27 20:47:07 -0300

    Update tarefa2.ts

--- tarefa2.ts
+++ tarefa2.ts
@@ -1,8 +1,3 @@
-// Escreva um programa para criar uma Collection Set do tipo number. 
-// O programa deverá solicitar ao usuário, que ele digite via teclado 10 valores
-//  inteiros não repetidos e adicione-os individualmente na Collection Set. 
-// Em seguida, faça o que se pede:
-// Mostre na tela todos os elementos da Collection Set. 
 
 import leia = require('readline-sync');
 
