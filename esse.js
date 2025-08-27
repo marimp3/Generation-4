6cd192899751400d3a67081f4e5eb135cfbb401f
Mariana Santana <marimoon1996@outlook.com>
2025-08-27 20:47:53 -0300

    Update esse.js

--- esse.js
+++ esse.js
@@ -1,18 +1,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
-var readline_sync_1 = require("readline-sync");
-var listaCores = [];
-var cores;
-for (var i = 0; i < 5; i++) {
+let readline_sync_1 = require("readline-sync");
+let listaCores = [];
+let cores;
+for (let i = 0; i < 5; i++) {
     cores = readline_sync_1.default.question("Digite uma cor: ");
     listaCores.push(cores);
 }
 console.log("\nCores cadastradas:");
-for (var i = 0; i < listaCores.length; i++) {
+for (let i = 0; i < listaCores.length; i++) {
     console.log(listaCores[i]);
 }
 
 listaCores.sort();
 console.log("\nCores cadastradas em ordem crescente:");
-for (var i = 0; i < listaCores.length; i++) {
+for (let i = 0; i < listaCores.length; i++) {
     console.log(listaCores[i]);
 }
